fb73a1861c0ab421879b4aa592a26bf9585475bb
1170300328 <48019291+1170300328@users.noreply.github.com>
2019-07-06 14:13:16 +0800

    Revert "Update HelloWorld.java"

--- HelloWorld/src/Main/HelloWorld.java
+++ HelloWorld/src/Main/HelloWorld.java
@@ -3,6 +3,5 @@ package Main;
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-        System.out.println("I really don't know how to change it, so I add a sentence here!");
   }
 }
